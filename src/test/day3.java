60f8775f7796d51facf1dbd9d3a5796f1924dbc3
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-02-03 02:21:28 -0300

    Section 14. 69. Priorizacion de test cases con TestNG

---
+++ src/test/day3.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day3 {
+
+	@Test
+	public void WebLoginCarLoan() {
+		//selenium
+		System.out.println("WebLoginCarLoan");
+
+	}
+	@Test
+	public void MobileLoginCarLoan() {
+		//Appium
+		System.out.println("MobileLoginCarLoan");
+
+	}
+	@Test
+	public void LoginAPICarLoan() {
+		//Rest API automatiom
+		System.out.println("LoginAPICarLoan");
+
+	}
+
+}
---
+++ src/test/day4.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day4 {
+
+	@Test
+	public void WebLoginHomeLoan() {
+		//selenium
+		System.out.println("WebLoginHomeLoan");
+
+	}
+	@Test
+	public void MobileLoginHomeLoan() {
+		//Appium
+		System.out.println("MobileLoginHomeLoan");
+
+	}
+	@Test
+	public void LoginAPIHomeLoan() {
+		//Rest API automatiom
+		System.out.println("LoginAPIHomeLoan");
+
+	}
+
+}
