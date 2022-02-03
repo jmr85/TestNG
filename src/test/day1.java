c3cbdd198bfd691ed7a3afdc7c03a9c389403093
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-02-03 01:43:05 -0300

    Section 14. 68. Configuracion del archivo testng.xml

--- src/test/day1.java
+++ src/test/day1.java
@@ -7,7 +7,13 @@ public class day1 {
 	@Test
 	public void Demo() {
 		System.out.println("Hello");
-		
+
+	}
+
+	@Test
+	public void SecondTest() {
+		System.out.println("Bye");
+
 	}
 
 }
---
+++ src/test/day2.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+
+	@Test
+	public void ploan() {
+		System.out.println("good");
+
+	}
+
+}
