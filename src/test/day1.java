0ace70735151200fbe9e4770dcd88d06f992dc5b
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-02-04 07:29:50 -0300

    Section 14. 72. TestNg Annotations part - 1

--- src/test/day1.java
+++ src/test/day1.java
@@ -1,9 +1,15 @@
 package test;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class day1 {
-
+	
+	@AfterTest
+	public void lastexecution() {
+		System.out.println("I will execute last");
+	}
+	
 	@Test
 	public void Demo() {
 		System.out.println("Hello");
--- src/test/day2.java
+++ src/test/day2.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class day2 {
@@ -8,6 +9,11 @@ public class day2 {
 	public void ploan() {
 		System.out.println("good");
 
+	}
+	
+	@BeforeTest
+	public void prerequisite() {
+		System.out.println("I will execute first");
 	}
 
 }
