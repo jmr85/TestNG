1f4a33e3556a3d2705a88e974b8482563e8f265d
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-02-05 00:24:28 -0300

    Section 14. 73. TestNG Annotations part - 2

--- src/test/day1.java
+++ src/test/day1.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
@@ -7,13 +8,18 @@ public class day1 {
 	
 	@AfterTest
 	public void lastexecution() {
-		System.out.println("I will execute last");
+		System.out.println(" I will execute last");
 	}
 	
 	@Test
 	public void Demo() {
 		System.out.println("Hello");
 
+	}
+	
+	@AfterSuite
+	public void afSuite() {
+		System.out.println(" I am the no 1 from last");
 	}
 
 	@Test
--- src/test/day3.java
+++ src/test/day3.java
@@ -1,5 +1,8 @@
 package test;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class day3 {
@@ -10,6 +13,21 @@ public class day3 {
 		System.out.println("WebLoginCarLoan");
 
 	}
+	
+	@BeforeMethod
+	public void beforeEvery() {
+		System.out.println(" I will execute before every test method in day 3 class");
+	}
+	@AfterMethod
+	public void afterEvery() {
+		System.out.println(" I will execute after every test method in day 3 class");
+	}
+	
+	@BeforeSuite
+	public void BfSuite() {
+		System.out.println("I am no 1");
+	}
+	
 	@Test
 	public void MobileSignInCarLoan() {
 		//Appium
