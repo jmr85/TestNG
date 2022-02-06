691df58a1530dca256a180c430aecb9f3ea1ee6a
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2022-02-06 04:12:53 -0300

    Section 14. 77. DataProvider Annotation - Casos de prueba parametrizados - 1

--- src/test/day3.java
+++ src/test/day3.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -60,5 +61,25 @@ public class day3 {
 		System.out.println("APICarLoan");
 
 	}
-
+	
+	@DataProvider
+	public Object[][] getData() {
+		//1st combination - username password - good credit card= row
+		//2nd username password - no credit history
+		//3rd fraudelent credit history
+		Object[][] data = new Object[3][2];
+		//1st set 
+		data[0][0]="firstsetusername";
+		data[0][1]="firstpassword";
+		//columns in the row are nothing but values for that particular combination(row)
+				
+		//2nd set
+		data[1][0]= "secondsetusername";
+		data[1][1]= "secondpassword";
+				
+		//3rd set
+		data[2][0]="thirdsetusername";
+		data[2][1]="thirdpassword";
+		return data;
+	}
 }
